ff4afd94f73f618331c09544d872a892d7bc6837
aaaa1ass <aaaa1ass@gmail.com>
2023-08-22 15:59:40 +0900

    특수문자 출력하기

---
+++ practice/대소문자 바꿔서 출력하기.h
@@ -0,0 +1,19 @@
+#pragma once
+//대소문자 바꿔서 출력하기
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    string str;
+    cin >> str;
+    for (int i = 0; i < str.size(); i++)
+    {
+        if ('a' <= str[i] && str[i] <= 'z')
+            cout << (char)(str[i] - 32);
+        else
+            cout << (char)(str[i] + 32);
+    }
+    return 0;
+}
