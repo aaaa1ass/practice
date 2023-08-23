ec96893e3e0ffcdebf3e1c102d8ad54044ea7439
aaaa1ass <aaaa1ass@gmail.com>
2023-08-23 16:29:19 +0900

    문자열 붙여서 출력하기

---
+++ practice/덧셈식 출력하기.h
@@ -0,0 +1,14 @@
+#pragma once
+//µ¡¼À½Ä Ãâ·ÂÇÏ±â
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int a;
+    int b;
+    cin >> a >> b;
+    cout << a << " + " << b << " = " << a + b << endl;
+    return 0;
+}
+
--- practice/소스.cpp
+++ practice/소스.cpp
@@ -1,12 +1,12 @@
-//덧셈식 출력하기
+//문자열 붙여서 출력하기
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main(void) {
-    int a;
-    int b;
-    cin >> a >> b;
-    cout << a << " + " << b << " = " << a + b << endl;
+    string str1, str2;
+    cin >> str1 >> str2;
+    cout << str1 << str2;
     return 0;
 }
