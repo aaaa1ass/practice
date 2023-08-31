a7fb310160018a311e25a377b80fcce1199216e0
aaaa1ass <aaaa1ass@gmail.com>
2023-08-31 18:18:15 +0900

    수열과 구간 쿼리 2

--- practice/소스.cpp
+++ practice/소스.cpp
@@ -1,4 +1,4 @@
-//수열과 구간 쿼리 3
+//수열과 구간 쿼리 2
 #include <iostream>
 #include <string>
 #include <vector>
@@ -6,11 +6,23 @@
 using namespace std;
 
 vector<int> solution(vector<int> arr, vector<vector<int>> queries) {
-    vector<int> answer = arr;
+    vector<int> answer;
     for (int i = 0; i < queries.size(); i++)
     {
-        swap(answer[queries[i][0]], answer[queries[i][1]]);
+        int min = 1000001;
+        for (int j = queries[i][0]; j <= queries[i][1]; j++)
+        {
+            if (arr[j] > queries[i][2] && arr[j] < min)
+            {
+                min = arr[j];
+            }
+        }
+        if (min == 1000001)
+            answer.push_back(-1);
+        else
+            answer.push_back(min);
     }
+
     return answer;
 }
 
@@ -18,9 +30,9 @@ using namespace std;
 
 int main() 
 {   
-    vector<int> arr = { 0, 1, 2, 3, 4 };
-    vector<vector<int>> q = { {0,3},{1,2},{1,4} };
-    for(int i = 0;i<arr.size();i++)
+    vector<int> arr = { 0, 1, 2, 4, 3 };
+    vector<vector<int>> q = { {0, 4, 2},{0, 3, 2},{0, 2, 2} };
+    for(int i = 0;i<q.size();i++)
         cout << solution(arr,q)[i];
     return 0;
 }
---
+++ practice/수열과 구간 쿼리 3.h
@@ -0,0 +1,27 @@
+#pragma once
+//수열과 구간 쿼리 3
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr, vector<vector<int>> queries) {
+    vector<int> answer = arr;
+    for (int i = 0; i < queries.size(); i++)
+    {
+        swap(answer[queries[i][0]], answer[queries[i][1]]);
+    }
+    return answer;
+}
+
+using namespace std;
+
+int main()
+{
+    vector<int> arr = { 0, 1, 2, 3, 4 };
+    vector<vector<int>> q = { {0,3},{1,2},{1,4} };
+    for (int i = 0; i < arr.size(); i++)
+        cout << solution(arr, q)[i];
+    return 0;
+}
